fbef3dff0c1d94a8ccb1eeed1f77885c8f354b9f
Haerfest <wouter.hobers@gmail.com>
2013-02-28 20:54:34 +0100

    KISS.

--- haskell/daily-verse.hs
+++ haskell/daily-verse.hs
@@ -7,27 +7,16 @@ import Network.HTTP
 
 -- Retrieves and prints the daily ESV verse.
 main :: IO ()
-main = do
-  simpleHTTP (getRequest buildURL) >>= getResponseBody >>= putStrLn
-
-
--- Returns the URL for the ESV daily verse.
-buildURL :: String
-buildURL = apiURL ++ "?" ++ optionsStr
+main = simpleHTTP (getRequest url) >>= getResponseBody >>= putStrLn
   where
-    apiURL       = "http://www.esvapi.org/v2/rest/dailyVerse"
-    optionsStr   = concat $ intersperse "&" optionsLst
-    optionsLst   = ["key=IP", "output-format=plain-text"] ++ disabledOpts
-    disabledOpts = disable ["include-first-verse-numbers",
-                            "include-verse-numbers",
-                            "include-footnotes",
-                            "include-short-copyright",
-                            "include-passage-horizontal-lines",
-                            "include-heading-horizontal-lines",
-                            "include-headings",
-                            "include-subheadings"]
-
-
--- Given a list of strings, returns a list where each string has "=false" appended.
-disable :: [String] -> [String]
-disable = map (++ "=false")
+    url = "http://www.esvapi.org/v2/rest/dailyVerse" ++ "?" ++
+          "key=IP"                                   ++ "&" ++
+          "output-format=plain-text"                 ++ "&" ++
+          "include-first-verse-numbers=false"        ++ "&" ++
+          "include-verse-numbers=false"              ++ "&" ++
+          "include-footnotes=false"                  ++ "&" ++
+          "include-short-copyright=false"            ++ "&" ++
+          "include-passage-horizontal-lines=false"   ++ "&" ++
+          "include-heading-horizontal-lines=false"   ++ "&" ++
+          "include-headings=false"                   ++ "&" ++
+          "include-subheadings=false"
