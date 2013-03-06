4714b7beae935dbd64069f9177ac77f1783585ac
Haerfest <wouter.hobers@gmail.com>
2013-03-06 22:12:42 +0100

    JSLint proof.

--- javascript/estimate-pi/estimate-pi.js
+++ javascript/estimate-pi/estimate-pi.js
@@ -4,11 +4,13 @@
  *
  * @return {boolean} True when the dart hit the quarter circle.
  */
-function throwOne () {
-    var x = Math.random();
-    var y = Math.random();
+function throwOne() {
+    'use strict';
 
-    return Math.sqrt(x*x + y*y) <= 1;
+    var x = Math.random(),
+        y = Math.random();
+
+    return Math.sqrt(x * x + y * y) <= 1;
 }
 
 
@@ -19,52 +21,17 @@ function throwOne () {
  *
  * @return {number} An estimation of pi.
  */
-function estimatePi (throwCount) {
-    var hitCount = 0;
-    
-    for (i = 0; i < throwCount; i++) {
+function estimatePi(throwCount) {
+    'use strict';
+
+    var hitCount = 0,
+        i;
+
+    for (i = 0; i < throwCount; i += 1) {
         if (throwOne()) {
-            hitCount++;
+            hitCount += 1;
         }
     }
 
     return 4 * hitCount / throwCount;
-}
-
-
-/**
- * Event handler to trigger an estimation of pi.
- *
- * Retrieves the number of throws from the element identified by 'throwCount',
- * estimates pi by throwing that many darts, and sets the value of the element
- * identified by 'pi' to the estimation.
- */
-function estimatePiEvent () {
-    var throwCount = document.getElementById('throwCount').value;
-    var pi         = estimatePi(throwCount);
-
-    document.getElementById('pi').value = pi;
-}
-
-
-/**
- * Returns whether the script is being run in a browser.
- *
- * @return {boolean} True when running in a browser.
- */
-function isRunningInBrowser () {
-    return typeof(document) != "undefined";
-}
-
-
-/**
- * If we run from the command-line (jsc on Mac OS X), expect
- * the number of throws as the only parameter, and print an
- * estimation of pi.
- */
-if (!isRunningInBrowser()) {
-    var throwCount = arguments[0] ? parseInt(arguments[0]) : 1000;
-    var pi         = estimatePi(throwCount);
-
-    print('pi = ' + pi);
 }
