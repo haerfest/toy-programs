691342ce7ed1cae7590371586fbef910ea872848
Haerfest <wouter.hobers@gmail.com>
2013-03-13 15:32:15 +0100

    Example to retrieve daily ESV verse.

---
+++ javascript/daily-verse/daily-verse.js
@@ -0,0 +1,43 @@
+/**
+ * Fetches the daily ESV verse via AJAX.
+ *
+ * @param {function} callback  Will be called with the daily verse text.
+ *
+ * @note This example may fail on certain browsers because of CORS (Cross
+ *       Origin Resource Sharing) limitations (Firefox, Chrome) or because
+ *       of a different AJAX API (older versions of Internet Explorer).
+ */
+function fetchDailyVerse(callback)
+{
+    var url = 'http://www.esvapi.org/v2/rest/dailyVerse' + '?' +
+              'key=IP'                                   + '&' +
+              'output-format=plain-text'                 + '&' +
+              'include-first-verse-numbers=false'        + '&' +
+              'include-verse-numbers=false'              + '&' +
+              'include-footnotes=false'                  + '&' +
+              'include-short-copyright=false'            + '&' +
+              'include-passage-horizontal-lines=false'   + '&' +
+              'include-heading-horizontal-lines=false'   + '&' +
+              'include-headings=false'                   + '&' +
+              'include-subheadings=false';
+
+    var httpRequest;
+
+    httpRequest = new XMLHttpRequest();
+    httpRequest.onreadystatechange = function() {
+        // Verify that the request is complete (4) and successful (200).
+        if (httpRequest.readyState === 4 && httpRequest.status === 200) {
+            callback(httpRequest.responseText);
+        }
+    };
+
+    // Make an asynchronous request (true).
+    httpRequest.open('GET', url, true);
+    httpRequest.send();
+}
+
+
+// Fetch the daily verse and update the DOM.
+fetchDailyVerse(function (text) {
+    document.getElementById('verse').innerText = text;
+});
