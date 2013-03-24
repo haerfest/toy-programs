e93627d36b3b01bde41fcf82a3b72ecf9ababeea
Haerfest <wouter.hobers@gmail.com>
2013-03-24 14:20:51 +0100

    Server static webpage.

--- javascript/node-test/server.js
+++ javascript/node-test/server.js
@@ -1,10 +1,21 @@
-var http = require('http'),
-    port = 8080,
-    ip   = '127.0.0.1';
+var sys   = require('sys'),
+    http  = require('http'),
+    fs    = require('fs'),
+    port  = 8080,
+    ip    = '127.0.0.1',
+    index;
+
+fs.readFile('index.html', function (err, data) {
+    if (err) {
+        throw err;
+    }
+    index = data;
+});
 
 http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-type': 'text/plain'});
-    res.end('Hello, world!');
+    res.writeHead(200, {'Content-type': 'text/html'});
+    res.write(index);
+    res.end();
 }).listen(port, ip);
 
 console.log('Server listening at ' + ip + ':' + port);
