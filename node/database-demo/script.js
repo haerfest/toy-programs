2aba002a99e33c9209a24d872e1b2188a5fe5a6d
Haerfest <wouter.hobers@gmail.com>
2013-04-17 17:31:36 +0200

    Add prev/next button.

--- node/database-demo/script.js
+++ node/database-demo/script.js
@@ -3,8 +3,25 @@ $(document).ready(function () {
     var regnum          = $("#regnum");
     var rec_time_micros = $("#rec_time_micros");
     var image           = $("#image");
+    var prev            = $("#prev");
+    var next            = $("#next");
 
-    rec_time_micros.attr('disabled', true);
+    prev.click(function () {
+        $.get('api/prev-event', {'rec_time_micros': rec_time_micros.val()}, function (data) {
+            rec_time_micros.empty();
+            rec_time_micros.append($('<option></option>').attr("value", data.rec_time_micros).text(data.title));
+            rec_time_micros.trigger('change');
+        });
+    });
+
+    next.click(function () {
+        $.get('api/next-event', {'rec_time_micros': rec_time_micros.val()}, function (data) {
+            rec_time_micros.empty();
+            rec_time_micros.append($('<option></option>').attr("value", data.rec_time_micros).text(data.title));
+            rec_time_micros.trigger('change');
+        });
+    });
+
     rec_time_micros.change(function () {
         if (this.value) {
             $.get('api/event-at-time', {'rec_time_micros': this.value}, function (data) {
