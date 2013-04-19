8adba17adc72c8c1bee9e0421d2d9cb14498e2a0
Haerfest <wouter.hobers@gmail.com>
2013-04-19 15:10:53 +0200

    Fetch images via path, add tarmac drawing

--- node/database-demo/script.js
+++ node/database-demo/script.js
@@ -1,33 +1,175 @@
-$(document).ready(function () {
+$(function () {
+
+    //
+    // Shortcuts for various elements.
+    //
+    var regnum          = $("#regnum");
+    var rec_time_micros = $("#rec_time_micros");
+    var images          = $("#images");
+    var slider          = $("#slider");
+
+    //
+    // Get a reference to the 2D context of the canvas and its dimensions.
+    //
+    var canvas    = $("#canvas")[0],
+        ctx       = canvas.getContext("2d"),
+        width     = canvas.width,
+        height    = canvas.height,
+        width_m   = 2 * 24,
+        height_m  = 2 * 6,
+        alpr_m    = 12,
+        marker_px = 7;
+
+    var drawTarmac = function () {
+
+        ctx.save();
+ 
+        //
+        // Fill it with a tarmac-like color.
+        //
+        ctx.fillStyle = "#333";
+        ctx.fillRect(0, 0, width, height);
+
+        //
+        // Draw the ALPR markers.
+        //
+        ctx.strokeStyle = "yellow";
+        ctx.beginPath();
+        ctx.moveTo(width / 2 - width * alpr_m / width_m, 0);
+        ctx.lineTo(width / 2 - width * alpr_m / width_m, height);
+        ctx.moveTo(width / 2 + width * alpr_m / width_m, 0);
+        ctx.lineTo(width / 2 + width * alpr_m / width_m, height);
+        ctx.stroke();
+
+        //
+        // Prepare to draw the axis.
+        //
+        ctx.strokeStyle = "#777";
+        ctx.lineWidth = 1;
+        ctx.beginPath();
+
+        //
+        // Draw horizontal axis.
+        //
+        ctx.moveTo(0, height / 2);
+        ctx.lineTo(width, height / 2);
+
+        //
+        // Draw horizontal markers.
+        //
+        for (var x = 0; x <= width / 2; x += width / width_m) {
+            ctx.moveTo(width / 2 - x, height / 2 - marker_px / 2);
+            ctx.lineTo(width / 2 - x, height / 2 + marker_px / 2);
+            ctx.moveTo(width / 2 + x, height / 2 - marker_px / 2);
+            ctx.lineTo(width / 2 + x, height / 2 + marker_px / 2);
+        }
+        
+        //
+        // Draw vertical axis.
+        //
+        ctx.moveTo(width / 2, 0);
+        ctx.lineTo(width / 2, height);
+
+        //
+        // Draw vertical markers.
+        //
+        for (var y = 0; y <= height / 2; y += height / height_m) {
+            ctx.moveTo(width / 2 - marker_px / 2, height / 2 - y);
+            ctx.lineTo(width / 2 + marker_px / 2, height / 2 - y);
+            ctx.moveTo(width / 2 - marker_px / 2, height / 2 + y);
+            ctx.lineTo(width / 2 + marker_px / 2, height / 2 + y);
+        }
+        ctx.stroke();
+
+        //
+        // Axis legendas.
+        //
+        ctx.font = "10px Arial";
+        ctx.fillStyle = "#999";
+        
+        ctx.textBaseline = "top";    
+        ctx.textAlign = "right";
+        ctx.fillText("x +" + (width_m / 2), width, height / 2 + marker_px / 2);
+        ctx.textAlign = "left";
+        ctx.fillText("-" + (width_m / 2), 0, height / 2 + marker_px / 2);
+
+        ctx.textAlign = "right";
+        ctx.textBaseline = "top";
+        ctx.fillText("y +" + (height_m / 2), width / 2 - marker_px / 2, 0);
+        ctx.textBaseline = "bottom";
+        ctx.fillText("-" + (height_m / 2), width / 2 - marker_px / 2, height);
+
+        ctx.restore();
+    };
+
+    drawTarmac();
+
+    //
+    // Set the canvas origin to the center.
+    //
+    ctx.translate(width / 2, height / 2);
+
+    //
+    // Scale it to be able to use physical units (meters) and
+    // orient the x-axis horizontally increasing to the right, and
+    // the y-axis vertically increasing upwards.
+    //
+    ctx.scale(width / width_m , -height / height_m);
+
+    //
+    // Draw a red circle in the center.
+    //
+    ctx.strokeStyle = "red";
+    ctx.lineWidth = 0.1;
+    ctx.beginPath();
+    ctx.arc(0, 0, 0.1, 0, 2 * Math.PI, true);
+    ctx.stroke();
+
+    //
+    // And a cyan one at x = 10m and y = 4 m.
+    //
+    ctx.strokeStyle = "cyan";
+    ctx.beginPath();
+    ctx.arc(10, 4, 0.1, 0, 2 * Math.PI, true);
+    ctx.stroke();
 
     //
     // Show all events that occurred at or right before #rec_time_micros.
     //
     var showEventsAtTime = function (rec_time_micros) {
-        $("#slider").slider("disable");
+        slider.slider("disable");
 
         $.getJSON('api/get-events-at-time',
                   {'rec_time_micros': rec_time_micros},
                   function (data) {
                       for (var index in data) {
-                          var id    = "image" + data[index].asset_management_id;
-                          var image = $("#" + id);
+                          var asset_id = "image" + data[index].asset_management_id;
+                          var image    = $("#" + asset_id);
                 
                           if (image.length === 0) {
                               var div = $("<div/>");
-                              image   = $("<img/>", {"id": id, "style": "width:100%"});
+                              image   = $("<img/>", {"id": asset_id, "style": "width:100%"});
                               
                               image.appendTo(div);
                               div.appendTo(images);
                           }
 
-                          image.attr('src', 'data:image/jpeg;base64,' + data[index].image);
+                          image.attr('src', "api/get-image/" + data[index].id);
                           image.attr('hidden', false);
                       }
-                      $("#slider").slider("enable");
+                      slider.slider("enable");
                   });
     };
 
+    //
+    // Update the slider if the user selects a new time from the drop-down box.
+    //
+    rec_time_micros.change(function() {
+        if (this.value) {
+            slider.slider("value", this.value);
+        }
+    });
+    
     //
     // Fetch time range and initialize time slider.
     //
@@ -39,55 +181,33 @@ $(document).ready(function () {
         $("#slider_max_value").text(new Date(maximum / 1E3));
         $("#slider_value").text(new Date(minimum / 1E3));
  
-        $("#slider").slider({ min: minimum ,
-                              max: maximum,
-                              slide: function (event, ui) {
-                                  $("#slider_value").text(new Date(ui.value / 1E3));
-                              },
-                              change: function (event, ui) {
-                                  showEventsAtTime(ui.value);
-                              }});
+        slider.slider({ min: minimum ,
+                        max: maximum,
+                        slide: function (event, ui) {
+                            $("#slider_value").text(new Date(ui.value / 1E3));
+                        },
+                        change: function (event, ui) {
+                            $("#slider_value").text(new Date(ui.value / 1E3));
+                            showEventsAtTime(ui.value);
+                        }});
     });
-
-    var regnum          = $("#regnum");
-    var rec_time_micros = $("#rec_time_micros");
-    var prev            = $("#prev");
-    var next            = $("#next");
-    var images          = $("#images");
-
-    prev.click(function () {
-        $.get('api/prev-event', {'rec_time_micros': rec_time_micros.val()}, function (data) {
-            rec_time_micros.empty();
-            rec_time_micros.append($('<option></option>').attr("value", data.rec_time_micros).text(data.title));
-            rec_time_micros.trigger('change');
-        });
-    });
-
-    next.click(function () {
-        $.get('api/next-event', {'rec_time_micros': rec_time_micros.val()}, function (data) {
-            rec_time_micros.empty();
-            rec_time_micros.append($('<option></option>').attr("value", data.rec_time_micros).text(data.title));
-            rec_time_micros.trigger('change');
-        });
-    });
-
 
     regnum.keydown(function (e) {
         var code = e.which;
 
         if (code === 13) {
             e.preventDefault();
-            regnum.attr('disabled', true);
+            regnum.attr("disabled", true);
+            slider.slider("disable");
             
             $.getJSON('api/find-regnum', {'regnum': regnum.val()}, function (data) {
                 rec_time_micros.empty();
                 $.each(data, function () {
-                    rec_time_micros.append($('<option></option>').attr("value", this.rec_time_micros).text(this.title));
+                    rec_time_micros.append($('<option></option>').attr("value", this.rec_time_micros).text(new Date(this.rec_time_micros / 1E3)));
                 });
-                rec_time_micros.trigger('change');
-
-                regnum.removeAttr('disabled');
-                rec_time_micros.removeAttr('disabled');
+                regnum.attr("disabled", false);
+                slider.slider("enable");
+                rec_time_micros.trigger("change");
             });
         }
     });
--- node/database-demo/server.js
+++ node/database-demo/server.js
@@ -39,28 +39,23 @@ app.get("/script.js", function (req, res) {
     returnFile(req.url.substr(1), res, "text/javascript");
 });
 
-app.get("/api/find-regnum", function (req, res) {
-    var param = req.query.regnum;
-    db.all("select strftime('%Y-%m-%d %H:%S:%f', rec_time_micros / 1E6, 'unixepoch') as title, rec_time_micros from pdsiitevent where licence_plate = ?", param, function (err, rows) {
-        res.writeHead(200, {"Content-Type": "text/json"});
-        res.end(JSON.stringify(rows));
-    });
-});
-
 
 function returnImages (req, res, json, asset_management_ids) {
     if (asset_management_ids.length === 0) {
         res.writeHead(200, {"Content-Type": "text/html"});
         res.end(JSON.stringify(json));
     } else {
-        db.get("select image from pdsiitevent where asset_management_id = ? and rec_time_micros <= ? order by rec_time_micros desc limit 1",
+        //
+        // We should fetch the last record before #rec_time_micros, but as that's very slow, we'll pick
+        // the first one after #rec_time_micros. We do no order as it is ordered by time.
+        //
+        db.get("select id from pdsiitevent where asset_management_id = ? and rec_time_micros >= ? limit 1",
                asset_management_ids[0],
                req.query.rec_time_micros,
                function (err, row) {
-                   if (!err) {
-                       var base64 = new Buffer(row.image).toString("base64");
+                   if (!err && row) {
                        json.push({"asset_management_id": asset_management_ids[0],
-                                  "image": base64});
+                                  "id": row.id});
                    }
                    
                    returnImages(req, res, json, asset_management_ids.slice(1));
@@ -68,6 +63,14 @@ function returnImages (req, res, json, asset_management_ids) {
     }
 }
 
+app.get("/api/get-image/:id", function (req, res) {
+    db.get("select image from pdsiitevent where id = ?", req.param("id"), function (err, row) {
+        if (!err && row) {
+            res.writeHead(200, {"Content-Type": "image/jpeg"});
+            res.end(row.image, "binary");
+        }
+    });
+});
 
 app.get("/api/get-events-at-time", function (req, res) {
     db.all("select distinct asset_management_id from pdsiitevent order by asset_management_id",
@@ -77,26 +80,17 @@ app.get("/api/get-events-at-time", function (req, res) {
            });
 });
 
-app.get("/api/next-event", function (req, res) {
-    var param = req.query.rec_time_micros;
-    db.each("select strftime('%Y-%m-%d %H:%S:%f', rec_time_micros / 1E6, 'unixepoch') as title, rec_time_micros from pdsiitevent where rec_time_micros > ? order by rec_time_micros asc limit 1", param, function (err, row) {
-        res.writeHead(200, {"Content-Type": "text/json"});
-        res.end(JSON.stringify(row));
-    });
-});
-
-app.get("/api/prev-event", function (req, res) {
-    var param = req.query.rec_time_micros;
-    db.each("select strftime('%Y-%m-%d %H:%S:%f', rec_time_micros / 1E6, 'unixepoch') as title, rec_time_micros from pdsiitevent where rec_time_micros < ? order by rec_time_micros desc limit 1", param, function (err, row) {
-        res.writeHead(200, {"Content-Type": "text/json"});
-        res.end(JSON.stringify(row));
-    });
-});
-
 app.get("/api/get-time-range", function (req, res) {
     db.get("select min(rec_time_micros) as minimum, max(rec_time_micros) as maximum from (select rec_time_micros from pdsiitevent union select rec_time_micros from pdstrackerevent)", function (err, row) {
         res.writeHead(200, {"Content-Type": "text/json"});
         res.end(JSON.stringify(row));
+    });
+});
+
+app.get("/api/find-regnum", function (req, res) {
+    db.all("select rec_time_micros from pdsiitevent where licence_plate = ?", req.query.regnum, function (err, rows) {
+        res.writeHead(200, {"Content-Type": "text/json"});
+        res.end(JSON.stringify(rows));        
     });
 });
 
