c68c26f4ffcfb3711ec1ab3fed5ddfefa1a81e62
Haerfest <wouter.hobers@gmail.com>
2013-04-26 22:49:08 +0200

    Allow starting time.

--- c++/opencv-demo/main.cpp
+++ c++/opencv-demo/main.cpp
@@ -7,27 +7,30 @@ using namespace cv;
 
 
 // Forward declarations.
-static void playVideo (const string video_file);
+static void playVideo (const string video_file, const unsigned int start_seconds);
 
 
 // Main program.
 int main (int argc, char *argv[])
 {
-  // We need a video file as the only parameter.
-  if (argc != 2) {
+  // We need a video file as the first parameter.
+  if (argc == 1) {
     cerr << "Usage: " << argv[0] << " <video-file>" << endl;
     return 1;
   }
 
+  // And optionally a seek time as the second position.
+  const unsigned int start_seconds = (argc == 3 ? atoi(argv[2]) : 0);
+
   // Play the video.
-  playVideo(argv[1]);
+  playVideo(argv[1], start_seconds);
 
   return 0;
 }
 
 
 // Plays a video file.
-static void playVideo (const string video_file)
+static void playVideo (const string video_file, const unsigned int start_seconds = 0)
 {
   VideoCapture capture(video_file);
   
@@ -39,7 +42,12 @@ static void playVideo (const string video_file)
   // Retrieve the frame rate.
   const double frame_rate        = capture.get(CV_CAP_PROP_FPS);
   const int    inter_frame_delay = 1000 / frame_rate;
-  
+
+  // Seek to a startion position, if we have one.
+  if (start_seconds > 0) {
+    (void) capture.set(CV_CAP_PROP_POS_MSEC, start_seconds * 1000);
+  }
+
   // Show the video.
   const string window_title = "Video";
   const int    escape_key   = 27;
@@ -57,5 +65,3 @@ static void playVideo (const string video_file)
   // Release the video.
   capture.release();
 }
-
-
