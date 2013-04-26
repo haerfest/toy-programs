d62ddb3bd38e419c50feb88b61c6b0becbd01c6c
Haerfest <wouter.hobers@gmail.com>
2013-04-26 23:35:53 +0200

    Prepare for Gaussians.

--- c++/opencv-demo/main.cpp
+++ c++/opencv-demo/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 #include <opencv2/opencv.hpp>
 
 
@@ -6,13 +7,22 @@ using namespace std;
 using namespace cv;
 
 
+// Types.
+typedef struct {
+  double mean;
+  double standard_deviation;
+  double weight;
+} Gaussian;
+
+typedef vector<Gaussian> GaussianMixture;
+
+
 // Forward declarations.
 static void playVideo (const string video_file, const unsigned int start_seconds);
 
 
 // Main program.
-int main (int argc, char *argv[])
-{
+int main (int argc, char *argv[]) {
   // We need a video file as the first parameter.
   if (argc == 1) {
     cerr << "Usage: " << argv[0] << " <video-file>" << endl;
@@ -30,8 +40,8 @@ int main (int argc, char *argv[])
 
 
 // Plays a video file.
-static void playVideo (const string video_file, const unsigned int start_seconds = 0)
-{
+static void playVideo (const string video_file, const unsigned int start_seconds = 0) {
+  Mat          image;
   VideoCapture capture(video_file);
   
   if (!capture.isOpened()) {
@@ -43,20 +53,26 @@ static void playVideo (const string video_file, const unsigned int start_seconds
   const double frame_rate        = capture.get(CV_CAP_PROP_FPS);
   const int    inter_frame_delay = 1000 / frame_rate;
 
-  // Seek to a startion position, if we have one.
-  if (start_seconds > 0) {
-    (void) capture.set(CV_CAP_PROP_POS_MSEC, start_seconds * 1000);
+  // Read the first frame to know the image size.
+  if (!capture.read(image)) {
+    cerr << "Could not read a frame from the video" << endl;
+    return;
   }
+    
+  // Seek to the startion position.
+  (void) capture.set(CV_CAP_PROP_POS_MSEC, start_seconds * 1000);
 
   // Show the video.
-  const string window_title = "Video";
-  const int    escape_key   = 27;
-  Mat          image;
-  Mat          colored_image;
+  const int       escape_key   = 27;
+  Mat             colored_image;
+  GaussianMixture gaussian_mixture[image.rows][image.cols];
   
   while (capture.read(image)) {
+    imshow("Input", image);
+    
     applyColorMap(image, colored_image, COLORMAP_JET);
-    imshow(window_title, colored_image);
+    imshow("Colormap", colored_image);
+    
     if (waitKey(inter_frame_delay) == escape_key) {
       break;
     }
