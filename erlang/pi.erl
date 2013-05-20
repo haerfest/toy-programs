418285ef79929f046f72e76629d6d387ab0891c7
Haerfest <wouter.hobers@gmail.com>
2013-05-20 13:54:22 +0200

    Add one-shot message passing.

--- erlang/pi.erl
+++ erlang/pi.erl
@@ -1,28 +1,45 @@
--module(pi).
--export([estimate/1]).
+% Estimate pi by throwing darts at a quarter of a dart board
+% with radius 1.
+%
+% Example usage:
+%
+% > c(pi).
+% > Pid = pi:start().
+% > pi:estimate(Pid, 1000000).
+% 3.1414
 
-% Throws one dart at a quarter circle of radius one
-% and returns whether the dart hit.
+-module(pi).
+-compile(export_all).
+
 throw_dart() ->
   X = random:uniform(),
   Y = random:uniform(),
   X * X + Y * Y =< 1.
 
-% Throws a number of darts at a quarter circle of radius
-% one and returns a list where element i indicates whether
-% throw i was a hit or not.
 throw_darts(0) ->
   [];
 throw_darts(Count) when Count > 0 ->
   Hit = throw_dart(),
   [Hit | throw_darts(Count - 1)].
 
-% Estimates pi by throwing darts at a quarter circle
-% of radius 1. The ratio between the darts that hit
-% the quarter circle and those that don't, are an
-% estimation of a quarter pi.
-estimate(ThrowCount) when ThrowCount > 0 ->
+estimate_pi(ThrowCount) when ThrowCount > 0 ->
   Throws   = throw_darts(ThrowCount),
   Hits     = [X || X <- Throws, X],
   HitCount = length(Hits),
   4 * HitCount / ThrowCount.
+
+estimator() ->
+  receive
+    {From, ThrowCount} ->
+      Pi = estimate_pi(ThrowCount),
+      From ! Pi
+  end.
+
+start() ->
+  spawn(?MODULE, estimator, []).
+
+estimate(Pid, ThrowCount) ->
+  Pid ! {self(), ThrowCount},
+  receive
+    Pi -> Pi
+  end.
