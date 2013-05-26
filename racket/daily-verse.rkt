13bde181c213fe497adfa3f4deb69bc05ed26432
Haerfest <wouter.hobers@gmail.com>
2013-05-26 23:43:31 +0200

    Fix output.

--- racket/daily-verse.rkt
+++ racket/daily-verse.rkt
@@ -1,7 +1,9 @@
+#!/usr/bin/env racket
+
 #lang racket
 
 (require net/url)
 
 (let* ([url   "http://www.esvapi.org/v2/rest/dailyVerse?key=IP&output-format=plain-text&include-first-verse-numbers=false&include-verse-numbers=false&include-footnotes=false&include-short-copyright=false&include-passage-horizontal-lines=false&include-heading-horizontal-lines=false&include-headings=false&include-subheadings=false"]
-       [verse (port->bytes (get-pure-port (string->url url)))])
-  (display verse))
+       [verse (bytes->string/utf-8 (port->bytes (get-pure-port (string->url url))))])
+  (display (string-append verse "\n")))
