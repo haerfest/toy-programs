3c7a84bc3af557b3c1ab08b27586474c2a2ccc26
Haerfest <wouter.hobers@gmail.com>
2013-06-13 00:06:50 +0200

    Updated solution to Project Euler #14.

--- clojure/project-euler-014.clj
+++ clojure/project-euler-014.clj
@@ -1,34 +1,30 @@
 (defn answer [upper-limit]
 
-  (let [memory (atom (transient {1 1}))]
+  (letfn [(collatz-len [n history mem]
+            (if-let [len (get mem n)]
+              ; have memoized n already, update history
+              (loop [len     len
+                     mem     mem
+                     history history]
+                (if (empty? history)
+                  [len mem]
+                  (recur (inc len)
+                         (assoc! mem (first history) len)
+                         (rest history))))
+              ; not memoized, recurse
+              (recur (if (even? n)
+                       (/ n 2)
+                       (inc (* 3 n)))
+                     (conj history n)
+                     mem)))]
 
-    (letfn [(collatz [n history mem]
-              (if-let [len (get mem n)]
-                ; have memoized n already, update history
-                (loop [len len
-                       mem mem
-                       history history]
-                  (if (empty? history)
-                    [len mem]
-                    (recur (inc len) (assoc! mem (first history) len) (rest history))))
-                ; not memoized, recurse
-                (recur (if (even? n)
-                         (/ n 2)
-                         (inc (* 3 n)))
-                       (conj history n)
-                       mem)))
-
-            (collatz-len [n]
-              (let [[len mem] (collatz n '() @memory)]
-                (swap! memory (fn [_] mem))
-                len))]
-
-      (loop [n (dec upper-limit)
-             winner 1
-             winner-len 1]
-        (if (zero? n)
-          winner
-          (let [len (collatz-len n)]
-            (if (> len winner-len)
-              (recur (dec n) n len)
-              (recur (dec n) winner winner-len))))))))
+    (loop [n          (dec upper-limit)
+           winner     1
+           winner-len 1
+           mem        (transient {1 1})]
+      (if (zero? n)
+        winner
+        (let [[len mem] (collatz-len n '() mem)]
+          (if (> len winner-len)
+            (recur (dec n) n      len        mem)
+            (recur (dec n) winner winner-len mem)))))))
