a3dbdfbc15896613f6993de674dd27a78db642b6
Haerfest <wouter.hobers@gmail.com>
2013-06-12 23:42:24 +0200

    Updated solution to Project Euler #14.

--- clojure/project-euler-014.clj
+++ clojure/project-euler-014.clj
@@ -1,26 +1,33 @@
 (defn answer [upper-limit]
 
-  (let [mem (atom {1 1})]
+  (let [memory (atom (transient {1 1}))]
 
-    (letfn [(collatz-len [n history]
-              (if-let [len (get @mem n)]
+    (letfn [(collatz [n history mem]
+              (if-let [len (get mem n)]
                 ; have memoized n already, update history
-                (reduce (fn [len n] (let [len (inc len)]
-                                      (swap! mem assoc n len)
-                                      len))
-                        len history)
+                (reduce (fn [[len mem] n]
+                          (let [len (inc len)]
+                            [len (assoc! mem n len)]))
+                        [len mem]
+                        history)
                 ; not memoized, recurse
                 (recur (if (even? n)
                          (/ n 2)
                          (inc (* 3 n)))
-                       (conj history n))))]
+                       (conj history n)
+                       mem)))
+
+            (collatz-len [n]
+              (let [[len mem] (collatz n '() @memory)]
+                (swap! memory (fn [_] mem))
+                len))]
 
       (loop [n (dec upper-limit)
              winner 1
              winner-len 1]
         (if (zero? n)
           winner
-          (let [len (collatz-len n '())]
+          (let [len (collatz-len n)]
             (if (> len winner-len)
               (recur (dec n) n len)
               (recur (dec n) winner winner-len))))))))
