456e5149894767699582be8f7981d3db13a8104e
Haerfest <wouter.hobers@gmail.com>
2013-08-29 20:05:14 +0200

    Half-way through Land of Lisp chapter 17.

---
+++ lisp/lol/svg.lisp
@@ -0,0 +1,101 @@
+;;; For some reason, compiling the entire file from Slime (C-c C-k) results in
+;;; the error that COMMON-LISP-USER::PAIRS cannot be found during the
+;;; macro-expansion of TAG.  No idea why that is.  First compiling LET1, SPLIT
+;;; and PAIRS manually (C-c C-c on each body) and then compiling the entire
+;;; file works fine.  Go figure.
+
+(defmacro let1 (var val &body body)
+  `(let ((,var ,val))
+     ,@body))
+
+(defmacro split (val yes no)
+  (let1 g (gensym)
+    `(let1 ,g ,val
+       (if ,g
+           (let ((head (car ,g))
+                 (tail (cdr ,g)))
+             ,yes)
+           ,no))))
+
+(defun pairs (lst)
+  (labels ((f (lst acc)
+             (split lst
+                    (if tail
+                        (f (cdr tail) (cons (cons head (car tail)) acc))
+                        (reverse acc))
+                    (reverse acc))))
+    (f lst nil)))
+
+(defun print-tag (name alst closingp)
+  (princ #\<)
+  (when closingp
+    (princ #\/))
+  (princ (string-downcase name))
+  (mapc (lambda (att)
+          (format t " ~a=\"~a\"" (string-downcase (car att)) (cdr att)))
+        alst)
+  (princ #\>))
+
+(defmacro tag (name atts &body body)
+  `(progn (print-tag ',name
+                     (list ,@(mapcar (lambda (x)
+                                       `(cons ',(car x) ,(cdr x)))
+                                     (pairs atts)))
+                     nil)
+          ,@body
+          (print-tag ',name nil t)))
+
+(defun brightness (col amt)
+  (mapcar (lambda (x)
+            (min 255 (max 0 (+ x amt))))
+          col))
+
+(defun svg-style (color)
+  (format nil
+          "~{fill:rgb(~a,~a,~a);stroke:rgb(~a,~a,~a)~}"
+          (append color
+                  (brightness color -100))))
+
+(defun circle (center radius color)
+  (tag circle (cx (car center)
+               cy (cdr center)
+               r radius
+               style (svg-style color))))
+
+(defun polygon (points color)
+  (tag polygon (points (format nil
+                               "~{~a,~a ~}"
+                               (mapcan (lambda (tp)
+                                         (list (car tp) (cdr tp)))
+                                       points))
+                       style (svg-style color))))
+
+(defun random-walk (value length)
+  (unless (zerop length)
+    (cons value
+          (random-walk (if (zerop (random 2))
+                           (1- value)
+                           (1+ value))
+                       (1- length)))))
+
+(defmacro svg (width height &body body)
+  `(tag svg (xmlns "http://www.w3.org/2000/svg"
+                   "xmlns:xlink" "http://www.w3.org/1999/xlink"
+                   height ,height
+                   width ,width)
+     ,@body))
+
+(defun demo ()
+  (with-open-file (*standard-output*
+                   "random_walk.svg"
+                   :direction :output
+                   :if-exists :supersede)
+    (svg 400 200
+      (loop repeat 10
+         do (polygon (append '((0 . 200))
+                             (loop for x ; CLisp-only, SBCL requires 'from 0'
+                                for y in (random-walk 100 400)
+                                collect (cons x y))
+                             '((400 . 200)))
+                     (loop repeat 3
+                          collect (random 256)))))))
