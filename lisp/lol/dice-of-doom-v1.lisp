7b9ac403600e326b6d1567869455239aa48a15c4
Haerfest <wouter.hobers@gmail.com>
2013-08-30 13:37:28 +0200

    Half way through Land of Lisp chapter 18.

--- lisp/lol/dice-of-doom-v1.lisp
+++ lisp/lol/dice-of-doom-v1.lisp
@@ -1,6 +1,6 @@
 (defparameter *num-players* 2)
 (defparameter *max-dice* 3)
-(defparameter *board-size* 2)
+(defparameter *board-size* 3)
 (defparameter *board-hexnum* (* *board-size* *board-size*))
 
 (setf *random-state* (make-random-state t))
