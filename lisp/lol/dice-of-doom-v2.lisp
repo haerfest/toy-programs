fd2a3ab9f754a64e0759baaeaa9fd79d8c241800
Haerfest <wouter.hobers@gmail.com>
2013-08-30 15:32:45 +0200

    Use lazy evaluation to play 4x4 against a computer.

--- lisp/lol/dice-of-doom-v2.lisp
+++ lisp/lol/dice-of-doom-v2.lisp
@@ -73,3 +73,70 @@
       (play-vs-human (handle-human tree))
       (announce-winner (cadr tree))))
 
+(defun limit-tree-depth (tree depth)
+  "Trims the search tree to a particular depth."
+  (list (car tree)
+        (cadr tree)
+        (if (zerop depth)
+            (lazy-nil)
+            (lazy-mapcar (lambda (move)
+                           (list (car move)
+                                 (limit-tree-depth (cadr move) (1- depth))))
+                         (caddr tree)))))
+
+(defparameter *ai-level* 4)
+
+(defun handle-computer (tree)
+  "Let's a computer player select a possible move and returns that move."
+  (let ((ratings (get-ratings (limit-tree-depth tree *ai-level*)
+                              (car tree))))
+    (cadr (lazy-nth (position (apply #'max ratings) ratings)
+                    (caddr tree)))))
+
+(defun play-vs-computer (tree)
+  "Plays the game against a computer player.  Example usage:
+  (play-vs-computer (game-tree (gen-board) 0 0 t))."
+  (print-info tree)
+  (cond ((lazy-null (caddr tree)) (announce-winner (cadr tree)))
+        ((zerop (car tree)) (play-vs-computer (handle-human tree)))
+        (t (play-vs-computer (handle-computer tree)))))
+
+(defun score-board (board player)
+  "Returns a score of board for player."
+  (loop for hex across board
+     for pos from 0
+     sum (if (eq (car hex) player)
+             (if (threatened pos board)
+                 1
+                 2)
+             -1)))
+
+(defun threatened (pos board)
+  "Returns whether player is threatened by a neighbor with more dice."
+  (let* ((hex    (aref board pos))
+         (player (car hex))
+         (dice   (cadr hex)))
+    (loop for n in (neighbors pos)
+       do (let* ((nhex    (aref board n))
+                 (nplayer (car nhex))
+                 (ndice   (cadr nhex)))
+            (when (and (not (eq player nplayer))
+                       (> ndice dice)
+                       (return t)))))))
+
+(defun get-ratings (tree player)
+  "Returns a list with rates for all moves a player can make."
+  (take-all (lazy-mapcar (lambda (move)
+                           (rate-position (cadr move) player))
+                         (caddr tree))))
+
+(defun rate-position (tree player)
+  "Returns a rating for a player's game position, using the minimax algorithm."
+  (let ((moves (caddr tree)))
+    (if (not (lazy-null moves))
+        (apply (if (eq (car tree) player)
+                   #'max
+                   #'min)
+               (get-ratings tree player))
+        (score-board (cadr tree) player))))
+
