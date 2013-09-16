b8c0f51a0ae20fed9475af33cc388f82757e5f6f
Haerfest <wouter.hobers@gmail.com>
2013-09-17 00:12:42 +0200

    Refactoring.

--- racket/daily-verse.rkt
+++ racket/daily-verse.rkt
@@ -1,9 +1,18 @@
-#!/usr/bin/env racket
-
-#lang racket
-
 (require net/url)
 
-(let* ([url   "http://www.esvapi.org/v2/rest/dailyVerse?key=IP&output-format=plain-text&include-first-verse-numbers=false&include-verse-numbers=false&include-footnotes=false&include-short-copyright=false&include-passage-horizontal-lines=false&include-heading-horizontal-lines=false&include-headings=false&include-subheadings=false"]
-       [verse (bytes->string/utf-8 (port->bytes (get-pure-port (string->url url))))])
-  (display (string-append verse "\n")))
+(define (get-daily-verse)
+  (let* ([s (string-append "http://www.esvapi.org/v2/rest/dailyVerse"
+                           "?key=IP"
+                           "&output-format=plain-text"
+                           "&include-first-verse-numbers=false"
+                           "&include-verse-numbers=false"
+                           "&include-footnotes=false"
+                           "&include-short-copyright=false"
+                           "&include-passage-horizontal-lines=false"
+                           "&include-heading-horizontal-lines=false"
+                           "&include-headings=false"
+                           "&include-subheadings=false")])
+    (bytes->string/utf-8 (port->bytes (get-pure-port (string->url s))))))
+
+(define (display-daily-verse)
+  (display (string-append (get-daily-verse) "\n")))
