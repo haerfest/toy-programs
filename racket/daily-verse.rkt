7cc22f7d72035773887b737658635e1aa13abfe0
Haerfest <wouter.hobers@gmail.com>
2013-09-21 19:01:14 +0200

    Support command-line usage.

--- racket/daily-verse.rkt
+++ racket/daily-verse.rkt
@@ -1,18 +1,22 @@
-(require net/url)
+#!/usr/bin/env racket -tm
 
-(define (get-daily-verse)
-  (let ([s (string-append "http://www.esvapi.org/v2/rest/dailyVerse"
-                          "?key=IP"
-                          "&output-format=plain-text"
-                          "&include-first-verse-numbers=false"
-                          "&include-verse-numbers=false"
-                          "&include-footnotes=false"
-                          "&include-short-copyright=false"
-                          "&include-passage-horizontal-lines=false"
-                          "&include-heading-horizontal-lines=false"
-                          "&include-headings=false"
-                          "&include-subheadings=false")])
-    (bytes->string/utf-8 (port->bytes (get-pure-port (string->url s))))))
+(module daily-verse racket
+  (provide main)
+  (require net/url)
 
-(define (display-daily-verse)
-  (displayln (get-daily-verse)))
+  (define (get-daily-verse)
+    (let ([s (string-append "http://www.esvapi.org/v2/rest/dailyVerse"
+                            "?key=IP"
+                            "&output-format=plain-text"
+                            "&include-first-verse-numbers=false"
+                            "&include-verse-numbers=false"
+                            "&include-footnotes=false"
+                            "&include-short-copyright=false"
+                            "&include-passage-horizontal-lines=false"
+                            "&include-heading-horizontal-lines=false"
+                            "&include-headings=false"
+                            "&include-subheadings=false")])
+      (bytes->string/utf-8 (port->bytes (get-pure-port (string->url s))))))
+
+  (define (main)
+    (displayln (get-daily-verse))))
