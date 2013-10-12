3aca391f9acf52a8eefd68a04230ea829560bea9
Haerfest <wouter.hobers@gmail.com>
2013-10-12 18:38:01 +0200

    Add answer P9-10.

--- haskell/99-questions.hs
+++ haskell/99-questions.hs
@@ -1,26 +1,26 @@
 {- 99 questions in Haskell
    http://www.haskell.org/haskellwiki/99_questions -}
 
-import Data.List (concat)
+import Data.List (concat, group)
 
--- Q1. Find the last element of a list.
+-- P01. Find the last element of a list.
 myLast = last
 
--- Q2. Find the last but one element of a list.
+-- P02. Find the last but one element of a list.
 myButLast = last . init
 
--- Q3. Find the K'th element of a list.  The first element in the list is
---     number 1.
+-- P03. Find the K'th element of a list.  The first element in the list is
+--      number 1.
 elementAt xs k = xs !! (k - 1)
 
--- Q4. Find the number of elements of a list.
+-- P04. Find the number of elements of a list.
 myLength = length
 
--- Q5. Reverse a list.
+-- P05. Reverse a list.
 myReverse = reverse
 
--- Q6. Find out whether a list is a palindrome. A palindrome can be read
---     forward or backward; e.g. (x a m a x).
+-- P06. Find out whether a list is a palindrome.  A palindrome can be read
+--      forward or backward; e.g. (x a m a x).
 isPalindrome :: Eq a => [a] -> Bool
 isPalindrome []  = True
 isPalindrome [_] = True
@@ -29,21 +29,33 @@ isPalindrome xs
   | otherwise           = False
   where middle = init $ tail xs
 
--- Q7. Flatten a nested list structure.
---     Transform a list, possibly holding lists as elements into a `flat' list
---     by replacing each list with its elements (recursively).
+-- P07. Flatten a nested list structure.  Transform a list, possibly holding
+--      lists as elements into a `flat' list by replacing each list with its
+--      elements (recursively).
 data NestedList a = Elem a | List [NestedList a]
 flatten :: NestedList a -> [a]
 flatten (Elem x)  = [x]
 flatten (List xs) = concat $ map flatten xs
 
--- Q8. Eliminate consecutive duplicates of list elements.
---     If a list contains repeated elements they should be replaced with a
---     single copy of the element. The order of the elements should not be
---     changed.
+-- P08. Eliminate consecutive duplicates of list elements.  If a list contains
+--      repeated elements they should be replaced with a single copy of the
+--      element.  The order of the elements should not be changed.
 compress :: Eq a => [a] -> [a]
 compress []  = []
 compress [x] = [x]
 compress (x:y:zs)
   | x == y     = compress (y:zs)
   | otherwise  = [x] ++ compress (y:zs)
+
+-- P09. Pack consecutive duplicates of list elements into sublists.  If a list
+--      contains repeated elements they should be placed in separate sublists.
+pack :: Eq a => [a] -> [[a]]
+pack = group
+
+-- P10. Run-length encoding of a list.  Use the result of problem P09 to
+--      implement the so-called run-length encoding data compression method.
+--      Consecutive duplicates of elements are encoded as lists (N E) where N is
+--      the number of duplicates of the element E.
+encode :: Eq a => [a] -> [(Int, a)]
+encode xs = map f $ pack xs
+  where f xs = (length xs, head xs)
