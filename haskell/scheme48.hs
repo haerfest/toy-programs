f311128aeb248106993b22b3bd94fd0aade7134d
Haerfest <wouter.hobers@gmail.com>
2013-10-27 14:37:10 +0100

    Finish chapter 6.

--- haskell/scheme48.hs
+++ haskell/scheme48.hs
@@ -7,6 +7,7 @@ import Control.Monad
 import Control.Monad.Error
 import Numeric
 import System.Environment
+import System.IO
 import Text.ParserCombinators.Parsec hiding (spaces)
 
 
@@ -71,7 +72,7 @@ showError (NotFunction message func)    = message ++ ": " ++ show func
 showError (UnboundVar message varname)  = message ++ ": " ++ varname
 
 
-trapError :: (Show e, MonadError e m) => m String -> m String
+trapError :: ThrowsError String -> ThrowsError String
 trapError action = catchError action (return . show)
 
 
@@ -335,12 +336,45 @@ eval badForm                               = throwError $ BadSpecialForm "Unreco
 
 
 {------------------------------------------------------------------------------}
+{-                                     REPL                                   -}
+{------------------------------------------------------------------------------}
+
+
+flushStr :: String -> IO ()
+flushStr str = putStr str >> hFlush stdout
+
+
+readPrompt :: String -> IO String
+readPrompt prompt = flushStr prompt >> getLine
+
+
+evalString :: String -> IO String
+evalString expr = return $ extractValue $ trapError (liftM show $ readExpr expr >>= eval)
+
+
+evalAndPrint :: String -> IO ()
+evalAndPrint expr = evalString expr >>= putStrLn
+
+
+until_ :: Monad m => (a -> Bool) -> m a -> (a -> m ()) -> m ()
+until_ pred prompt action = do result <- prompt
+                               if pred result
+                                 then return ()
+                                 else action result >> until_ pred prompt action
+
+
+runRepl :: IO ()
+runRepl = until_ (== "quit") (readPrompt "Lisp>>> ") evalAndPrint
+
+
+{------------------------------------------------------------------------------}
 {-                                     Main                                   -}
 {------------------------------------------------------------------------------}
 
 
 main :: IO ()
-main = do
-  args   <- getArgs
-  evaled <- return $ liftM show $ readExpr (args !! 0) >>= eval
-  putStrLn $ extractValue $ trapError evaled
+main = do args <- getArgs
+          case length args of
+            0 -> runRepl
+            1 -> evalAndPrint $ args !! 0
+            otherwise -> putStrLn "Program takes only 0 or 1 argument"
