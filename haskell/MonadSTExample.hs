caaae96bf485e340f0e2cb108a5c0467d73166de
Haerfest <wouter.hobers@gmail.com>
2013-10-29 12:36:57 +0100

    Add ST monad example.

---
+++ haskell/MonadSTExample.hs
@@ -0,0 +1,17 @@
+{- http://www.haskell.org/haskellwiki/Monad/ST -}
+
+module MonadSTExample (main) where
+
+import Control.Monad.IO.Class (liftIO)
+import Control.Monad.ST (runST)
+import Data.STRef (modifySTRef, newSTRef, readSTRef)
+import Control.Monad (forM_)
+
+sumST :: Num a => [a] -> a
+sumST xs = runST $ do
+  n <- newSTRef 0
+  forM_ xs $ \x -> modifySTRef n (+x)
+  readSTRef n
+
+main :: IO ()
+main = putStrLn $ show $ sumST [1..100]
