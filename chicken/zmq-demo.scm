02b1e43dcf00a1c60e4b15cb560a31f8a8ed15c9
Haerfest <wouter.hobers@gmail.com>
2013-12-24 17:19:32 +0100

    Refactoring.

--- chicken/zmq-demo.scm
+++ chicken/zmq-demo.scm
@@ -2,19 +2,19 @@
 (require-extension posix)
 
 (define (subscribe endpoint fn)
-
-  (define (receive-loop s)
-    (let* ([t (receive-message* s as: 'string)]
-           [m (if (socket-option s 'rcvmore)
-                  (receive-message* s as: 'blob)
-                  '())])
-      (fn t m)
-      (receive-loop s)))
-
   (let ([s (make-socket 'sub)])
     (connect-socket s endpoint)
     (socket-option-set! s 'subscribe "")
-    (receive-loop s)))
+
+    (define (receive-loop)
+      (let* ([t (receive-message* s as: 'string)]
+             [m (if (socket-option s 'rcvmore)
+                    (receive-message* s as: 'blob)
+                    '())])
+        (fn t m)
+        (receive-loop)))
+
+    (receive-loop)))
 
 (define (demo)
   (subscribe "tcp://10.18.129.51:55555"
@@ -22,4 +22,4 @@
                (display (format "~a: ~a (~a bytes)~%"
                                 (seconds->string (current-seconds))
                                 t
-                                (if (blob? m) (blob-size m) 0))))))
+                                (if (blob? m) (blob-size m) #\?))))))
