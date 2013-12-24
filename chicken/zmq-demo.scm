3536596b0a6eb55eb6444eb17b57a4cb822037bf
Haerfest <wouter.hobers@gmail.com>
2013-12-24 15:26:42 +0100

    Add ZMQ demo in Chicken Scheme.

---
+++ chicken/zmq-demo.scm
@@ -0,0 +1,27 @@
+(require-extension zmq)
+(require-extension posix)
+
+(define (subscribe endpoint fn)
+
+  (define (receive-loop s)
+    (let* ([t (receive-message* s as: 'string)]
+           [m (if (socket-option s 'rcvmore)
+                  (receive-message* s as: 'blob)
+                  '())])
+      (fn t m)
+      (receive-loop s)))
+
+  (let ([s (make-socket 'sub)])
+    (connect-socket s endpoint)
+    (socket-option-set! s 'subscribe "")
+    (receive-loop s)))
+
+(define (demo)
+  (subscribe "tcp://10.18.129.51:55555"
+             (lambda (t m)
+               (display (seconds->string (current-seconds)))
+               (display ": ")
+               (display t)
+               (newline))))
+
+(demo)
