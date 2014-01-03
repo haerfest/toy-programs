2bc5b2077535ac79cade2ebd1852bf57e96eb687
Haerfest <wouter.hobers@gmail.com>
2014-01-03 15:36:31 +0100

    Refactoring.

--- python/zmq-demo.py
+++ python/zmq-demo.py
@@ -4,27 +4,22 @@ import logging
 import zmq
 
 class Listener:
-    def __init__(self, addr):
-        self.addr = addr
-        self.ctx  = zmq.Context.instance()
-        self.sck  = self.ctx.socket(zmq.SUB)
+    def listen(self, addr, callback=None):
+        context = zmq.Context.instance()
+        socket  = context.socket(zmq.SUB)
 
-    def listen(self, callback=None):
-        self.sck.connect(self.addr)
-        self.sck.setsockopt(zmq.SUBSCRIBE, b'')
+        socket.connect(addr)
+        socket.setsockopt(zmq.SUBSCRIBE, b'')
+        
         while True:
-            parts = self.sck.recv_multipart()
-            if len(parts) == 2:
-                if callback:
-                    callback(parts[0], parts[1])
-            else:
-                Logger.error("Received %d parts, but expected 2" % len(parts))
+            topic, message = socket.recv_multipart()
+            if callback:
+                callback(topic, message)
 
-def foo(topic, msg):
+def print_topic(topic, message):
     print topic
 
-listener = Listener("tcp://10.18.129.51:55555")
-listener.listen(foo)
+Listener().listen("tcp://10.18.129.51:55555", print_topic)
 
             
         
