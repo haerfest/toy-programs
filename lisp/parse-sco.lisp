96128675f59d6cee9cf527dba2fd934177207d0d
Haerfest <wouter.hobers@gmail.com>
2014-01-14 15:43:23 +0100

    Refactoring.

--- lisp/parse-sco.lisp
+++ lisp/parse-sco.lisp
@@ -17,20 +17,20 @@
                  ((#\space #\tab #\newline #\return) (tok tokens)) ; skip whitespace and line endings
                  (#\# (skip-to '(#\newline #\return)) ; skip comment to end of line
                       (tok tokens))
-                 (#\{ (tok (cons 'block-begin tokens)))
-                 (#\} (tok (cons 'block-end tokens)))
+                 (#\{ (tok (cons 'begin tokens)))
+                 (#\} (tok (cons 'end tokens)))
                  (#\= (tok (cons 'equals tokens)))
-                 (#\" (tok (cons (cons 'value
+                 (#\" (tok (cons (cons 'val
                                        (coerce (skip-to '(#\") t) 'string))
                                  tokens))) ; read string until next double quote
-                 (t (tok (cons (cons 'identifier
+                 (t (tok (cons (cons 'id
                                      (coerce (cons c
                                                    (skip-to '(#\space #\tab #\newline #\return #\# #\{ #\} #\=)))
                                              'string))
                                tokens))))))) ; read identifier until whitepsace, line endings or other tokens
     (tok '())))
 
-(defun expect (expectations tokens)
+(defun expect (expectations tokens &optional (ids '()))
   "Matches expectations to tokens. Returns three values: success, remaining tokens, variables."
   (labels ((variablep (thing)
              (and (symbolp thing)
@@ -42,7 +42,7 @@
                    ((null tokens) (values nil nil ids)) ; no more tokens, but still expectations
                    ((and (atom expectations)
                          (atom tokens))
-                    (cond ((equal expectations tokens) (values t '() ids)) ; two equal atoms
+                    (cond ((equalp expectations tokens) (values t '() ids)) ; two equal atoms
                           ((variablep expectations) ; variable matches any atom
                            (values t '() (cons (cons (var-name expectations)
                                                      tokens)
@@ -55,19 +55,5 @@
                           (iter (cdr expectations) (cdr tokens) ids)
                           (values nil tokens ids)))) ; cons's don't match
                    (t (values nil tokens ids))))) ; atom and cons don't match
-    (iter expectations tokens '())))
+    (iter expectations tokens ids)))
 
-(defun demo ()
-  (let* ((sco          (format nil "foo~%{~%  bar = \"b.a.r.\"  # comment~%  baz~%  {~%    bax = \"b.a.x.\"  }~%}~%"))
-         (tokens       (with-input-from-string (stream sco)
-                         (tokenize stream)))
-         (expectations '((identifier . ?foo)
-                         block-begin
-                         (identifier . ?bar) equals (value . ?bar-val)
-                         (identifier . ?baz)
-                         block-begin
-                         (identifier . ?bax) equals (value . ?bax-val)
-                         block-end
-                         block-end)))
-    (princ tokens)
-    (expect expectations tokens)))
