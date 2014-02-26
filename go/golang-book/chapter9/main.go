2fc3f8107b6432efd1e0a1566c37987efce4c0b2
Wouter Hobers <wouter.hobers@gmail.com>
2014-02-26 13:10:19 +0100

    Update example.

--- go/golang-book/chapter9/main.go
+++ go/golang-book/chapter9/main.go
@@ -19,21 +19,59 @@ type Circle struct {
 }
 
 type Shape interface {
+	description() string
 	area() float64
+	perimeter() float64
+}
+
+// -------------------------------------------------------------------
+//  Square
+// -------------------------------------------------------------------
+func (this *Square) description() string {
+	return fmt.Sprintf("Square{ width: %d }", this.width)
 }
 
 func (this *Square) area() float64 {
 	return float64(this.width * this.width)
 }
 
+func (this *Square) perimeter() float64 {
+	return float64(4 * this.width)
+}
+
+// -------------------------------------------------------------------
+//  Rectangle
+// -------------------------------------------------------------------
+func (this *Rectangle) description() string {
+	return fmt.Sprintf("Rectangle{ width: %d, height: %d }", this.width, this.height)
+}
+
 func (this *Rectangle) area() float64 {
 	return float64(this.width * this.height)
+}
+
+func (this *Rectangle) perimeter() float64 {
+	return float64(2 * (this.width + this.height))
+}
+
+// -------------------------------------------------------------------
+//  Circle
+// -------------------------------------------------------------------
+func (this *Circle) description() string {
+	return fmt.Sprintf("Circle{ radius: %d }", this.radius)
 }
 
 func (this *Circle) area() float64 {
 	return math.Pi * float64(this.radius * this.radius)
 }
 
+func (this *Circle) perimeter() float64 {
+	return math.Pi * float64(2 * this.radius)
+}
+
+// -------------------------------------------------------------------
+//  Main
+// -------------------------------------------------------------------
 func totalArea(shapes ...Shape) float64 {
 	var area float64
 	for _, shape := range shapes {
@@ -42,10 +80,18 @@ func totalArea(shapes ...Shape) float64 {
 	return area
 }
 
+func printStats(shape Shape) {
+	fmt.Println(shape.description(), "area:", shape.area(), "perimeter:", shape.perimeter())
+}
+
 func main() {
 	square    := Square{ width: 10 }
 	rectangle := Rectangle{ width: 10, height: 20 }
 	circle    := Circle{ radius: 10 }
 
-	fmt.Println(totalArea(&square, &rectangle, &circle))
+	printStats(&square)
+	printStats(&rectangle)
+	printStats(&circle)
+
+	fmt.Println("Total area", totalArea(&square, &rectangle, &circle))
 }
