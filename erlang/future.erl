0352be75c8e1c90ceb6d833cfac3a1e394f6f629
Wouter Hobers <wouter.hobers@gmail.com>
2014-09-08 15:54:45 +0200

    Add comment.

--- erlang/future.erl
+++ erlang/future.erl
@@ -19,12 +19,20 @@ await(Future, Timeout) ->
             timeout
     end.
 
+%% Output:
+%%
+%% starting a 5 second future
+%% waiting a second...got timeout
+%% waiting until computed...got ok
+%% ok
 demo() ->
     io:format("starting a 5 second future~n", []),
     Future = future(fun() -> timer:sleep(5000), ok end),
+
     io:format("waiting a second...", []), 
     FirstResult = await(Future, 1000),
     io:format("got ~p~n", [FirstResult]),
+
     io:format("waiting until computed...", []),
     SecondResult = await(Future),
     io:format("got ~p~n", [SecondResult]).
