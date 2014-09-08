6663f739efe77122369e0d9a6b84b7df1a957734
Wouter Hobers <wouter.hobers@gmail.com>
2014-09-08 16:31:31 +0200

    Made futures work on other nodes.

--- erlang/future.erl
+++ erlang/future.erl
@@ -1,16 +1,26 @@
 -module(future).
--export([future/1, await/1, await/2, demo/0]).
+-export([future/1, future/2, await/1, await/2, demo/1, worker/2]).
 
+% Invokes a function and sends the result back to the caller.
+worker(Fn, Caller) ->
+    Caller ! {future, self(), Fn()}.
+
+% Executes a future on the local node.
 future(Fn) ->
-    Caller = self(),
-    spawn(fun() -> Caller ! {future, self(), Fn()} end).
+    spawn(worker, [Fn, self()]).
 
+% Executes a future on any node.
+future(Node, Fn) ->
+    spawn(Node, ?MODULE, worker, [Fn, self()]).
+
+% Waits for a future to complete.
 await(Future) ->
     receive
         {future, Future, Result} ->
             Result
     end.
 
+% Waits until a timeout for a future to complete.
 await(Future, Timeout) ->
     receive
         {future, Future, Result} ->
@@ -19,15 +29,10 @@ await(Future, Timeout) ->
             timeout
     end.
 
-%% Output:
-%%
-%% starting a 5 second future
-%% waiting a second...got timeout
-%% waiting until computed...got ok
-%% ok
-demo() ->
-    io:format("starting a 5 second future~n", []),
-    Future = future(fun() -> timer:sleep(5000), ok end),
+% Demonstrates running and waiting for futures.
+demo(Node) ->
+    io:format("starting a 5 second future on node ~p~n", [Node]),
+    Future = future(Node, fun() -> timer:sleep(5000), ok end),
 
     io:format("waiting a second...", []), 
     FirstResult = await(Future, 1000),
