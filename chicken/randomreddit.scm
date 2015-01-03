274efa600632bad5407e919020606b4d7f046a55
Wouter Hobers <wouter.hobers@gmail.com>
2015-01-03 19:13:48 +0100

    Rewrote in functional style.

--- chicken/randomreddit.scm
+++ chicken/randomreddit.scm
@@ -1,17 +1,22 @@
 (use http-client uri-common)
 
-(define (random-reddit)
-  (let-values (((html uri response) (with-input-from-request "http://www.reddit.com/r/random/" #f read-string)))
-    (display (uri->string uri))
-    (newline)))
-
-(define (random-reddits n)
-  (when (> n 0)
-        (random-reddit)
-        (random-reddits (- n 1))))
+(define (random-reddit n)
+  (define (iter n acc)
+    (if (zero? n)
+        acc
+        (let-values (((html uri response)
+                      (with-input-from-request "http://www.reddit.com/r/random/" #f read-string)))
+          (iter (- n 1)
+                (cons (uri->string uri) acc)))))
+  (iter n '()))
 
 (define (main args)
-  (unless (null? args)
-          (random-reddits (string->number (car args)))))
+  (let ((n (if (null? args)
+               1
+               (string->number (car args)))))
+    (map (lambda (s)
+           (display s)
+           (newline))
+         (random-reddit n))))
 
 (main (command-line-arguments))
