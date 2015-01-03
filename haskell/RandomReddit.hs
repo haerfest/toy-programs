86c1a84da253b72d4532593ae615afa401a198d5
Wouter Hobers <wouter.hobers@gmail.com>
2015-01-03 18:52:32 +0100

    Refactoring.

--- haskell/RandomReddit.hs
+++ haskell/RandomReddit.hs
@@ -1,21 +1,24 @@
 {-# LANGUAGE OverloadedStrings #-}
 
+import Control.Monad (liftM)
 import System.IO.Streams (InputStream, OutputStream, stdout)
 import Data.Maybe (fromJust)
 import qualified System.IO.Streams as Streams
 import qualified Data.ByteString.Char8 as S
 import Network.Http.Client
 
-main :: IO ()
-main = do
-  c <- openConnection "www.reddit.com" 80
+randomReddit :: Int -> IO [S.ByteString]
+randomReddit n = withConnection (openConnection "www.reddit.com" 80) fetch
+  where
+    fetch conn = do
+      req <- buildRequest $ do
+        http GET "/r/random/"
+        setAccept "text/html"
+      mapM (\_ -> do
+               sendRequest conn req emptyBody
+               receiveResponse conn
+                 (\resp _ -> return $ fromJust $ getHeader resp "location"))
+        [1..n]
 
-  q <- buildRequest $ do
-    http GET "/r/random/"
-    setAccept "text/html"
-
-  sendRequest c q emptyBody
-
-  receiveResponse c (\r _ -> S.putStrLn $ fromJust $ getHeader r "location")                        
-
-  closeConnection c
+main :: IO S.ByteString
+main = liftM head $ randomReddit 1
