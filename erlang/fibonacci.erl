72a66409cf00d7fb28e5a329fb0ee8204d0568d3
haerfest <wouter.hobers+github@gmail.com>
2015-12-13 18:25:09 +0100

    No need to spawn a separate process.

--- erlang/fibonacci.erl
+++ erlang/fibonacci.erl
@@ -7,8 +7,10 @@
 %%  > timer:tc(fibonacci, recursive, [40]).
 %%  {6390387,165580141}
 %%
-%%  > timer:tc(fibonacci, memoized, [40]).
-%%  {214,165580141}
+%% > timer:tc(fibonacci, memoized, [40]).
+%% {480,165580141}
+%% 2> timer:tc(fibonacci, memoized, [40]).
+%% {6,165580141}
 %% ----------------------------------------------------------------------------
 
 %% ----------------------------------------------------------------------------
@@ -21,60 +23,26 @@ recursive(N) when N > 1 ->
     recursive(N - 1) + recursive(N - 2).
 
 %% ----------------------------------------------------------------------------
-%%  Memoized version.  It spawns a separate process to maintain the state,
-%%  i.e. the (N, fib(N)) tuples we have calculated so far.
+%%  Memoized version.  It uses its own process dictionary to remember the
+%%  (N, fib(N)) tuples it has calculated so far.
 %% ----------------------------------------------------------------------------
 
 memoized(N) when N >= 0 ->
-    Memory = spawn(fun memory/0),
-    store(Memory, 0, 1),  % the two base
-    store(Memory, 1, 1),  % cases
-    M = fib(N, Memory),
-    stop(Memory),
-    M.
+    put(0, 1),
+    put(1, 1),
+    fib(N).
 
 %% ----------------------------------------------------------------------------
-%%  Little helper function that uses the memory process to calculate a
+%%  Little helper function that uses the process dictionary to calculate a
 %%  fibonacci(N) only once for a given N.
 %% ----------------------------------------------------------------------------
 
-fib(N, Memory) ->
-    case retrieve(Memory, N) of
+fib(N) ->
+    case get(N) of
         undefined ->
-            M = fib(N - 1, Memory) + fib(N - 2, Memory),
-            store(Memory, N, M),
+            M = fib(N - 1) + fib(N - 2),
+            put(N, M),
             M;
         M ->
             M
     end.
-
-%% ----------------------------------------------------------------------------
-%%  The memory process that keeps track of calculated fibonacci numbers.  It
-%%  uses a process's built-in dictionary to store them.  Note that the store
-%%  is generic, it has no notion of what is being calculated nor that anything
-%%  is calculated at all -- it only keeps track of (key, value) pairs.
-%% ----------------------------------------------------------------------------
-
-memory() ->
-    receive
-        {set, N, M} ->
-            put(N, M),
-            memory();
-        {get, N, Pid} ->
-            Pid ! get(N),
-            memory();
-        stop ->
-            ok
-    end.
-
-%% ----------------------------------------------------------------------------
-%%  Accessor functions to interact with the memory.
-%% ----------------------------------------------------------------------------
-
-store(Memory, N, M) -> Memory ! {set, N, M}.
-
-retrieve(Memory, N) ->
-    Memory ! {get, N, self()},
-    receive M -> M end.
-
-stop(Memory) -> Memory ! stop.
