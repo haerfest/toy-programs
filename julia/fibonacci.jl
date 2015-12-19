15bba1806c839968dddab6e413b2a4e26d0fddc9
haerfest <wouter.hobers+github@gmail.com>
2015-12-19 01:49:00 +0100

    Add memoization example.

--- julia/fibonacci.jl
+++ julia/fibonacci.jl
@@ -6,5 +6,27 @@ function fib(n)
     end
 end
 
+function memoizedfib(n)
+    memory = Dict{Int,Int}()
+
+    function internal(n)
+        if !haskey(memory, n)
+            memory[n] = fib(n - 2) + fib(n - 1)
+        end
+        return memory[n]
+    end
+
+    return internal(n)
+end
+
+# julia> include("fibonacci.jl")
+#   0.988681 seconds (1.15 k allocations: 58.018 KB)
+# 165580141
+#   0.990496 seconds (2.48 k allocations: 126.052 KB)
+# 165580141
+
 @time n = fib(40)
+println(n)
+
+@time n = memoizedfib(40)
 println(n)
