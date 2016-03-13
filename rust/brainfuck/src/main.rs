46be6588f95cfcca8f8386342613c223c0730ab5
haerfest <wouter.hobers+github@gmail.com>
2016-03-13 20:33:48 +0100

    Compiles.

--- rust/brainfuck/src/main.rs
+++ rust/brainfuck/src/main.rs
@@ -1,18 +1,33 @@
 use std::io::Read;
 
-struct Machine {
+struct Machine<'a> {
     mem: [u8; 30000],
     dp: usize,
-    code: String,
+    code: &'a [u8],
     pc: usize
 }
 
-fn execute(m: &mut Machine) {
-    let code = m.code.as_bytes();
-    let len = code.len();
+fn find(m: &mut Machine, needle: u8, skip: u8, forwards: bool) {
+    let mut depth = 0;
+    while !(m.code[m.pc] == needle && depth == 0) {
+        if m.code[m.pc] == needle {
+            depth -= 1;
+        } else if m.code[m.pc] == skip {
+            depth += 1;
+        }
 
+        if forwards {
+            m.pc += 1;
+        } else {
+            m.pc -= 1;
+        }
+    }
+}
+
+fn execute(m: &mut Machine) {
+    let len = m.code.len();
     while m.pc < len {
-        match code[m.pc] {
+        match m.code[m.pc] {
             b'+' => {
                 m.mem[m.dp] += 1;
                 m.pc += 1;
@@ -30,7 +45,7 @@ fn execute(m: &mut Machine) {
                 m.pc += 1;
             },
             b'.' => {
-                print!("{}", m.mem[m.dp]);
+                print!("{:?}", m.mem[m.dp]);
                 m.pc += 1;
             },
             b',' => {
@@ -41,19 +56,35 @@ fn execute(m: &mut Machine) {
                 };
                 m.pc += 1;
             },
+            b'[' => {
+                if m.mem[m.dp] == 0 {
+                    m.pc += 1;
+                    find(m, b']', b'[', true);
+                } else {
+                    m.pc += 1;
+                }
+            },
+            b']' => {
+                if m.mem[m.dp] == 0 {
+                    m.pc += 1;
+                } else {
+                    m.pc -= 1;
+                    find(m, b'[', b']', false);
+                }
+            },
             _ =>
-                panic!("invalid opcode {}", code[m.pc])
+                panic!("invalid opcode {:?}", m.code[m.pc])
         }
     }
 }
 
 fn main() {
-    let hello_world = "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.";
+    let hello_world = b"++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.";
     let mut m: Machine = Machine {
         mem: [0; 30000],
         dp: 0,
         pc: 0,
-        code: String::from(hello_world)
+        code: hello_world
     };
     execute(&mut m);
 }
