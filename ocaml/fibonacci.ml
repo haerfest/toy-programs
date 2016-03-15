fa9f5719f5e7fef688b0ac5fb43e4a510dbd95e4
haerfest <wouter.hobers+github@gmail.com>
2016-03-15 21:13:30 +0100

    Add Fibonacci.

---
+++ ocaml/fibonacci.ml
@@ -0,0 +1,13 @@
+(* To compile: ocamlbuild -lib Unix fibonacci.native *)
+
+let rec fib n =
+    if n < 2 then 1
+    else fib(n - 2) * fib(n - 1)
+
+let time f x =
+    let t = Unix.gettimeofday() in
+    let fx = f x in
+    Printf.printf "elapsed: %f sec\n" (Unix.gettimeofday() -. t);
+    fx
+
+let _ = time fib 40
