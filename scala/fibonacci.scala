8e82363491e10973f2e17a02301de03921ff85f8
haerfest <wouter.hobers+github@gmail.com>
2016-04-07 00:17:52 +0200

    Change into runnable class.

--- scala/fibonacci.scala
+++ scala/fibonacci.scala
@@ -1,12 +1,24 @@
-def fib(n: Int): Int = {
-  def inner(n: Int, a: Int, b: Int): Int = {
-    if (n == 0)
-      a
-    else
-      inner(n - 1, b, a + b)
-  }
-  inner(n, 0, 1)
-}
+object Application {
 
-val n = 41
-println(s"fib($n) = ${fib(n)}")
+  def fib(n: Int): Int = {
+    def inner(n: Int, a: Int, b: Int): Int = {
+      if (n == 0)
+        a
+      else
+        inner(n - 1, b, a + b)
+    }
+    inner(n, 0, 1)
+  }
+
+  def timeIt(f: () => Unit): Unit = {
+    val startTime = System.nanoTime()
+    f()
+    val endTime = System.nanoTime()
+    val elapsed = (endTime - startTime) / 1e6
+    println(s"elapsed: $elapsed msec");
+  }
+
+  def main(args: Array[String]): Unit = {
+    timeIt(() => println(fib(41)))
+  }
+}
