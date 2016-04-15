186ed20c3e7cee35efa354f67e160788775540e4
haerfest <wouter.hobers+github@gmail.com>
2016-04-15 23:29:21 +0200

    Add hello world example.

---
+++ c#/test.cs
@@ -0,0 +1,14 @@
+using System;
+
+// With mono:
+//
+//   $ mcs test.cs
+//   $ mono test.exe
+
+public class Application
+{
+    public static void Main()
+    {
+        Console.WriteLine("Hello, world!");
+    }
+}
