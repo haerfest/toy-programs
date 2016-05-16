94b6caa31087e2ab064fad3c265afd1129414029
haerfest <wouter.hobers+github@gmail.com>
2016-05-16 14:07:47 +0200

    Refactor.

--- python/gaspedaal.py
+++ python/gaspedaal.py
@@ -1,30 +1,53 @@
 from bs4 import BeautifulSoup
+from urllib.parse import quote_plus as q
 import requests
 
-base_url = 'http://www.gaspedaal.nl/Mazda/MX-5/'
-query = '?pmax=20000&kmax=80000&bmin=2005&trns=Handgeschakeld&srt=df'
 
-r = requests.get(base_url + query)
-if not r.status_code == 200:
-    raise Exception('Unexpected HTTP status code {}'.format(r.status_code))
+class Scraper:
 
-soup = BeautifulSoup(r.text, 'html.parser')
-cars = soup.find_all('div', 'item-desktop')
-if cars == 0:
-    raise Exception('Did not find any cars')
+    def scrape(self, brand, model, **kwargs):
+        """
+        Scrapes gaspedaal.nl for all ads for a particular car brand and model.
+        The search can be narrowed down by **kwargs. Cars found are returned as
+        a list of dictionaries.
+        """
+        url = 'http://www.gaspedaal.nl/{0}/{1}/'.format(q(brand), q(model))
 
-print("Found {} cars".format(len(cars)))
+        if len(kwargs) > 0:
+            pairs = ['{0}={1}'.format(q(k), q(str(v)))
+                     for k, v in kwargs.items()]
+            url += '?' + '&'.join(pairs)
 
-for car in cars:
-    try:
-        price = car.find('span', 'prijs').b.text
-        year = car.find('div', 'bj-col').contents[1]
-        mileage = car.find('div', 'km-col').contents[1]
-        title = car.find('div', 'titel-col').a.text
-        link = car.find('div', 'titel-col').a['href']
-        description = car.find('div', 'oms-col').text
+        r = requests.get(url)
+        if not r.status_code == 200:
+            raise Exception('HTTP status code {}'.format(r.status_code))
 
-        items = [year, mileage, price, title, description, link]
-        print("\t".join(items))
-    except:
-        pass
+        soup = BeautifulSoup(r.text, 'html.parser')
+        cars = soup.find_all('div', 'item-desktop')
+
+        return filter(lambda car: car is not None, map(self.extract, cars))
+
+    def extract(self, soup):
+        """
+        Extract a single car's information from HTML soup. Returns None if no
+        information could be extracted.
+        """
+        try:
+            price = soup.find('span', 'prijs').b.text
+            year = soup.find('div', 'bj-col').contents[1]
+            mileage = soup.find('div', 'km-col').contents[1]
+            title = soup.find('div', 'titel-col').a.text
+            link = soup.find('div', 'titel-col').a['href']
+            description = soup.find('div', 'oms-col').text
+
+            return {'year': year, 'mileage': mileage, 'price': price,
+                    'title': title, 'description': description, 'link': link}
+        except:
+            return None
+
+
+if __name__ == '__main__':
+    scraper = Scraper()
+    for car in scraper.scrape('Mazda', 'MX-5', pmax=20000, kmax=80000,
+                              bmin=2005, trns='Handgeschakeld'):
+        print(car)
