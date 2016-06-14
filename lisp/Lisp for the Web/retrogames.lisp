f1b8b10e83f0573deff2d45439d2852f27dabb1e
haerfest <wouter.hobers+github@gmail.com>
2016-06-14 21:17:51 +0200

    Following book.

--- lisp/Lisp for the Web/retrogames.lisp
+++ lisp/Lisp for the Web/retrogames.lisp
@@ -31,3 +31,47 @@
   (print-unreadable-object (object stream :type t)
     (with-slots (name votes) object
       (format stream "name: ~s with ~d votes" name votes))))
+
+(defmacro standard-page ((&key title) &body body)
+  `(with-html-output-to-string
+       (*standard-output* nil :prologue t :indent t)
+     (:html :lang "en"
+            (:head
+             (:meta :charset "utf-8")
+             (:title ,title)
+             (:link :type "text/css"
+                    :rel "stylesheet"
+                    :href "/retro.css"))
+            (:body
+             (:div :id "header"         ; retro games header
+                   (:img :src "/logo.png"
+                         :alt "Commodore 64"
+                         :class "logo")
+                   (:span :class "strapline"
+                          "Vote on your favourite Retro Game"))
+             ,@body))))
+
+(defun start-server (port)
+  (start (make-instance 'easy-acceptor :port port)))
+
+(define-easy-handler (retro-games :uri "/retro-games") ()
+  (standard-page (:title "Retro Games")
+    (:h1 "Vote on your all time favourite retro games!")
+    (:p "Missing a game? Make it available for votes "
+        (:a :href "new-game" "here"))
+    (:h2 "Current stand")
+    (:div :id "chart"
+          (:ol
+           (dolist (game (games))
+             (htm
+              (:li (:a :href (format nil "vote?name=~a"
+                                     (url-encode (name game)))
+                       "Vote!")
+                   (fmt "~A with ~d votes"
+                        (escape-string (name game))
+                        (votes game)))))))))
+
+(define-easy-handler (vote :uri "/vote") (name)
+  (when (game-stored? name)
+    (vote-for (game-from-name name)))
+  (redirect "/retro-games"))
