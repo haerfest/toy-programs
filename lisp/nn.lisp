dfb1fd47f43d38848454bae469a24d29febbf3ef
haerfest <wouter.hobers+github@gmail.com>
2018-07-27 15:42:50 +0200

    Add bias. Add third hidden neuron to XOR-network.

--- lisp/nn.lisp
+++ lisp/nn.lisp
@@ -1,4 +1,17 @@
 ;; Adapted from Artificial Intelligence A Modern Approach, ch. 18.7.4.
+;;
+;; Example:
+;; 
+;; CL-USER> (train xor-samples xor-network :learning-rate 0.5 :epochs 1000000)
+;; 4.3219616e-6
+;; CL-USER> (infer xor-network #(0 0))
+;; (0.0021414205)
+;; CL-USER> (infer xor-network #(0 1))
+;; (0.99683446)
+;; CL-USER> (infer xor-network #(1 0))
+;; (0.99683267)
+;; CL-USER> (infer xor-network #(1 1))
+;; (0.0029395432)
 
 (defun sigmoid (x)
   (/ 1.0 (+ 1.0 (exp (- x)))))
@@ -17,17 +30,36 @@
 
 (defstruct layer
   neurons
-  weights)
+  weights
+  bias)
 
 (defstruct network
   layers)
 
-(defparameter xor-network
+(defparameter matt-network
   (make-network :layers (vector (make-layer :neurons (vector (make-neuron) (make-neuron)))
                                 (make-layer :neurons (vector (make-neuron) (make-neuron))
-                                            :weights (make-array '(2 2)))
+                                            :weights (make-array '(2 2)
+                                                                 :initial-contents '((0.15 0.25)
+                                                                                     (0.20 0.30)))
+                                            :bias 0.35)
+                                (make-layer :neurons (vector (make-neuron) (make-neuron))
+                                            :weights (make-array '(2 2)
+                                                                 :initial-contents '((0.40 0.50)
+                                                                                     (0.45 0.55)))
+                                            :bias 0.60)))
+  "https://mattmazur.com/2015/03/17/a-step-by-step-backpropagation-example/")
+
+(defparameter matt-samples '((#(0.05 0.10) #(0.01 0.99))))
+
+(defparameter xor-network
+  (make-network :layers (vector (make-layer :neurons (vector (make-neuron) (make-neuron)))
+                                (make-layer :neurons (vector (make-neuron) (make-neuron) (make-neuron))
+                                            :weights (make-array '(2 3))
+                                            :bias 1.0)
                                 (make-layer :neurons (vector (make-neuron))
-                                            :weights (make-array '(2 1))))))
+                                            :weights (make-array '(3 1))
+                                            :bias 1.0))))
 
 (defparameter xor-samples '((#(0 0) #(0))
                             (#(0 1) #(1))
@@ -43,16 +75,17 @@
           (dotimes (j (array-dimension weights 1))
             (setf (aref weights i j) (- (random 1.0) 0.5))))))))
 
-(defun rmse (network)
+(defun squared-error (network)
   (let* ((output-layer (svref (network-layers network)
                               (- (length (network-layers network)) 1)))
          (n (length (layer-neurons output-layer)))
          (squared-error 0))
     (dotimes (j n)
       (let ((neuron-j (svref (layer-neurons output-layer) j)))
-        (incf squared-error (* (neuron-error neuron-j)
+        (incf squared-error (* 0.5
+                               (neuron-error neuron-j)
                                (neuron-error neuron-j)))))
-    (sqrt (/ squared-error n))))
+    squared-error))
 
 (defun set-input (network input)
   (let* ((input-layer (svref (network-layers network) 0)))
@@ -67,7 +100,7 @@
           (prev-layer (svref (network-layers network) (- index 1))))
       (dotimes (j (length (layer-neurons curr-layer)))
         (let ((neuron-j (svref (layer-neurons curr-layer) j)))
-          (setf (neuron-input neuron-j) 0)
+          (setf (neuron-input neuron-j) (layer-bias curr-layer))
           (dotimes (i (length (layer-neurons prev-layer)))
             (let ((neuron-i (svref (layer-neurons prev-layer) i)))
               (incf (neuron-input neuron-j)
@@ -81,7 +114,7 @@
   (let ((output-layer (svref (network-layers network)
                               (- (length (network-layers network)) 1))))
     (dotimes (j (length (layer-neurons output-layer)))
-      (let ((neuron-j (svref (layer-neurons output-layer) j)))
+      (let* ((neuron-j (svref (layer-neurons output-layer) j)))
         (setf (neuron-error neuron-j)
               (- (svref target j)
                  (neuron-output neuron-j)))
@@ -122,8 +155,9 @@
                        (neuron-output neuron-i)
                        (neuron-delta neuron-j))))))))))
 
-(defun train (samples network &key (learning-rate 0.001) (epochs 100) (callback nil))
-  (initialize-weights network)
+(defun train (samples network &key (learning-rate 0.001) (epochs 100) (callback nil) (init-weights t))
+  (when init-weights
+    (initialize-weights network))
   (dotimes (epoch epochs)
     (dolist (sample samples)
       (set-input network (first sample))
@@ -132,8 +166,8 @@
       (back-propagation network)
       (update-weights network learning-rate))
     (when callback
-      (funcall callback (+ epoch 1) (rmse network))))
-  (rmse network))
+      (funcall callback (+ epoch 1) (squared-error network))))
+  (squared-error network))
 
 (defun collect-output (network)
    (let ((output nil)
@@ -142,7 +176,7 @@
     (dotimes (j (length (layer-neurons output-layer)))
       (let ((neuron-j (svref (layer-neurons output-layer) j)))
         (push (neuron-output neuron-j) output)))
-    output))
+    (reverse output)))
 
 (defun infer (network input)
   (set-input network input)
