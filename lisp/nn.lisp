ca7c12008ec0a37da31c717a2e02a5be33525bb0
haerfest <wouter.hobers+github@gmail.com>
2018-07-28 14:04:59 +0200

    Rewrite using LOOP.

--- lisp/nn.lisp
+++ lisp/nn.lisp
@@ -65,116 +65,114 @@
                             (#(1 1) #(0))))
 
 (defun initialize-weights (network)
-  (dotimes (index (length (network-layers network)))
-    (when (> index 0)
-      (let* ((layer (svref (network-layers network) index))
-             (weights (layer-weights layer)))
-        (dotimes (i (array-dimension weights 0))
-          (dotimes (j (array-dimension weights 1))
-            (setf (aref weights i j) (- (random 1.0) 0.5))))))))
+  (loop
+     for index from 1 below (length (network-layers network))
+     for layer = (svref (network-layers network) index)
+     for weights = (layer-weights layer) do
+       (loop for i below (array-dimension weights 0) do
+            (loop for j below (array-dimension weights 1) do
+                 (setf (aref weights i j) (- (random 1.0) 0.5))))))
 
 (defun squared-error (network)
-  (let* ((output-layer (svref (network-layers network)
-                              (- (length (network-layers network)) 1)))
-         (n (length (layer-neurons output-layer)))
-         (squared-error 0))
-    (dotimes (j n)
-      (let ((neuron-j (svref (layer-neurons output-layer) j)))
-        (incf squared-error (* 0.5
-                               (neuron-error neuron-j)
-                               (neuron-error neuron-j)))))
-    squared-error))
+  (loop
+     with output-layer = (svref (network-layers network)
+                                (- (length (network-layers network)) 1))
+     for j below (length (layer-neurons output-layer))
+     for neuron-j = (svref (layer-neurons output-layer) j)
+     for error = (neuron-error neuron-j)
+     sum (/ (* error error) 2)))
 
 (defun set-input (network input)
-  (let* ((input-layer (svref (network-layers network) 0)))
-    (dotimes (i (length (layer-neurons input-layer)))
-      (let ((neuron-i (svref (layer-neurons input-layer) i)))
-        (setf (neuron-output neuron-i) (svref input i))))))
+  (loop
+     with input-layer = (svref (network-layers network) 0)
+     for i below (length (layer-neurons input-layer))
+     for neuron-i = (svref (layer-neurons input-layer) i) do
+       (setf (neuron-output neuron-i) (svref input i))))
 
 (defun forward-pass (network)
-  (do ((index 1 (1+ index)))
-      ((= index (length (network-layers network))))
-    (let ((curr-layer (svref (network-layers network) index))
-          (prev-layer (svref (network-layers network) (- index 1))))
-      (dotimes (j (length (layer-neurons curr-layer)))
-        (let ((neuron-j (svref (layer-neurons curr-layer) j)))
-          (setf (neuron-input neuron-j) (layer-bias curr-layer))
-          (dotimes (i (length (layer-neurons prev-layer)))
-            (let ((neuron-i (svref (layer-neurons prev-layer) i)))
-              (incf (neuron-input neuron-j)
-                    (* (aref (layer-weights curr-layer) i j)
-                       (neuron-output neuron-i)))))
-          (setf (neuron-output neuron-j)
-                (funcall (neuron-function neuron-j)
-                         (neuron-input neuron-j))))))))
+  (loop
+     for index from 1 below (length (network-layers network))
+     for curr-layer = (svref (network-layers network) index)
+     for prev-layer = (svref (network-layers network) (- index 1)) do
+       (loop
+          for j below (length (layer-neurons curr-layer))
+          for neuron-j = (svref (layer-neurons curr-layer) j) do
+            (setf (neuron-input neuron-j)
+                  (+ (layer-bias curr-layer)
+                     (loop
+                        for i below (length (layer-neurons prev-layer))
+                        for neuron-i = (svref (layer-neurons prev-layer) i)
+                        sum (* (aref (layer-weights curr-layer) i j)
+                               (neuron-output neuron-i)))))
+            (setf (neuron-output neuron-j)
+                  (funcall (neuron-function neuron-j)
+                           (neuron-input neuron-j))))))
 
 (defun calculate-errors (network target)
-  (let ((output-layer (svref (network-layers network)
-                              (- (length (network-layers network)) 1))))
-    (dotimes (j (length (layer-neurons output-layer)))
-      (let* ((neuron-j (svref (layer-neurons output-layer) j)))
-        (setf (neuron-error neuron-j)
-              (- (svref target j)
-                 (neuron-output neuron-j)))
-        (setf (neuron-delta neuron-j)
-              (* (funcall (neuron-derivative neuron-j)
-                          (neuron-input neuron-j))
-                 (neuron-error neuron-j)))))))
+  (loop
+     with output-layer = (svref (network-layers network)
+                                (- (length (network-layers network)) 1))
+     for j below (length (layer-neurons output-layer))
+     for neuron-j = (svref (layer-neurons output-layer) j) do
+       (setf (neuron-error neuron-j)
+             (- (svref target j) (neuron-output neuron-j)))
+       (setf (neuron-delta neuron-j)
+             (* (funcall (neuron-derivative neuron-j) (neuron-input neuron-j))
+                (neuron-error neuron-j)))))
 
 (defun back-propagation (network)
-  (do ((index (- (length (network-layers network)) 2) (1- index)))
-      ((zerop index))
-    (let ((curr-layer (svref (network-layers network) index))
-          (next-layer (svref (network-layers network) (+ index 1))))
-      (dotimes (i (length (layer-neurons curr-layer)))
-        (let ((neuron-i (svref (layer-neurons curr-layer) i))
-              (sum 0))
-          (dotimes (j (length (layer-neurons next-layer)))
-            (let ((neuron-j (svref (layer-neurons next-layer) j)))
-              (incf sum (* (aref (layer-weights next-layer) i j)
-                           (neuron-delta neuron-j)))))
-          (setf (neuron-delta neuron-i)
-                (* (funcall (neuron-derivative neuron-i)
-                            (neuron-input neuron-i))
-                   sum)))))))
+  (loop
+     for index from (- (length (network-layers network)) 2) downto 1
+     for curr-layer = (svref (network-layers network) index)
+     for next-layer = (svref (network-layers network) (+ index 1)) do
+       (loop
+          for i below (length (layer-neurons curr-layer))
+          for neuron-i = (svref (layer-neurons curr-layer) i) do
+            (setf (neuron-delta neuron-i)
+                  (* (funcall (neuron-derivative neuron-i)
+                              (neuron-input neuron-i))
+                     (loop
+                        for j below (length (layer-neurons next-layer))
+                        for neuron-j = (svref (layer-neurons next-layer) j)
+                        sum (* (aref (layer-weights next-layer) i j)
+                               (neuron-delta neuron-j))))))))
+
 
 (defun update-weights (network learning-rate)
-  (do ((index 1 (1+ index)))
-      ((= index (length (network-layers network))))
-    (let* ((curr-layer (svref (network-layers network) index))
-           (prev-layer (svref (network-layers network) (- index 1)))
-           (weights (layer-weights curr-layer)))
-      (dotimes (i (length (layer-neurons prev-layer)))                
-        (let ((neuron-i (svref (layer-neurons prev-layer) i)))
-          (dotimes (j (length (layer-neurons curr-layer)))
-            (let ((neuron-j (svref (layer-neurons curr-layer) j)))
-              (incf (aref weights i j)
-                    (* learning-rate
-                       (neuron-output neuron-i)
-                       (neuron-delta neuron-j))))))))))
+  (loop
+     for index from 1 below (length (network-layers network))
+     for prev-layer = (svref (network-layers network) (- index 1))
+     for curr-layer = (svref (network-layers network) index)
+     for weights = (layer-weights curr-layer) do
+       (loop
+          for i below (length (layer-neurons prev-layer))
+          for neuron-i = (svref (layer-neurons prev-layer) i) do
+            (loop
+               for j below (length (layer-neurons curr-layer))
+               for neuron-j = (svref (layer-neurons curr-layer) j) do
+                 (incf (aref weights i j)
+                       (* learning-rate
+                          (neuron-output neuron-i)
+                          (neuron-delta neuron-j)))))))
 
-(defun train (samples network &key (learning-rate 0.001) (epochs 100) (callback nil) (init-weights t))
-  (when init-weights
-    (initialize-weights network))
-  (dotimes (epoch epochs)
-    (dolist (sample samples)
-      (set-input network (first sample))
-      (forward-pass network)
-      (calculate-errors network (second sample))
-      (back-propagation network)
-      (update-weights network learning-rate))
-    (when callback
-      (funcall callback (+ epoch 1) (squared-error network))))
+(defun train (samples network &key (learning-rate 0.001) (epochs 100))
+  (initialize-weights network)
+  (loop repeat epochs do
+       (loop for (x y) in samples do
+            (set-input network x)
+            (forward-pass network)
+            (calculate-errors network y)
+            (back-propagation network)
+            (update-weights network learning-rate)))
   (squared-error network))
 
 (defun collect-output (network)
-   (let ((output nil)
-        (output-layer (svref (network-layers network)
-                              (- (length (network-layers network)) 1))))
-    (dotimes (j (length (layer-neurons output-layer)))
-      (let ((neuron-j (svref (layer-neurons output-layer) j)))
-        (push (neuron-output neuron-j) output)))
-    (reverse output)))
+  (loop
+     with output-layer = (svref (network-layers network)
+                                (- (length (network-layers network)) 1))
+     for j below (length (layer-neurons output-layer))
+     for neuron-j = (svref (layer-neurons output-layer) j)
+     collect (neuron-output neuron-j)))
 
 (defun infer (network input)
   (set-input network input)
