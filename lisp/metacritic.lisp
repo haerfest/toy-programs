baea39188e22b5d5f8b05af4c838fb20ea2a32b1
haerfest <wouter.hobers+github@gmail.com>
2019-11-02 11:30:31 +0100

    Parse HTML.

--- lisp/metacritic.lisp
+++ lisp/metacritic.lisp
@@ -1,11 +1,16 @@
 ;; (ql:quickload :drakma)
+;; (ql:quickload :plump)
+;; (ql:quickload :clss)
 
 ;; https://www.metacritic.com/search/game/majora%27s%20mask/results?plats[16]=1&search_type=advanced
 (defun get-scores (title)
     (multiple-value-bind (body status headers uri stream must-close reason)
         (drakma:http-request "https://www.metacritic.com/search/game/majora%27s%20mask/results"
                              :user-agent :explorer)
-      (with-open-file (stream "out.html"
-                              :direction :output
-                              :if-exists :supersede)
-        (format stream body))))
+      (let ((dom (plump:parse body)))
+        (loop for result across (clss:select "li.result" dom)
+           collect (list
+                    (plump:text (elt (clss:select "span.platform" result) 0))
+                    (string-trim '(#\Space #\t #\newline)
+                                 (plump:text (elt (clss:select "h3.product_title a" result) 0)))
+                    (plump:text (elt (clss:select "span.metascore_w" result) 0)))))))
